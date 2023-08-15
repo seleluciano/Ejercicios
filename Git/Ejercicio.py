d8f54973aad13254b0befc302884925fac5d7c24
seleluciano <seleluciano4@gmail.com>
2023-08-15 19:17:11 -0300

    Ejercicio Git

--- Git/Ejercicio.py
+++ Git/Ejercicio.py
@@ -28,21 +28,22 @@ def division(x,y):
 
 
 print("*****Elija la opcion*****")
-print("1 Ingresar valores \n 2 Sumar  \n 3 Restar  \n 4 Multiplicar \n 5 Division \n 6 Salir \n")
+print("1 Ingresar valores \n 3 Sumar  \n 4 Restar  \n 5 Multiplicar \n 6 Division \n 7 Salir \n")
 n=int(input("Elija una opcion"))
-while n!=6:
+while n!=7:
     if n==1:
         X= float(input("Ingrese un valor "))
-        Y= float(input("Ingrese otro valor "))
     elif n==2:
-        suma(X,Y)
+        Y= float(input("Ingrese otro valor "))
     elif n==3:
-        resta(X,Y)
+        suma(X,Y)
     elif n==4:
-        multiplicacion(X,Y)
+        resta(X,Y)
     elif n==5:
+        multiplicacion(X,Y)
+    elif n==6:
         division(X,Y)
     print("*****Elija la opcion*****")
-    print("1 Ingresar valores \n 2 Sumar  \n 3 Restar  \n 4 Multiplicar \n 5 Division \n 6 Salir \n")
+    print("1 Ingresar valores \n 3 Sumar  \n 4 Restar  \n 5 Multiplicar \n 6 Division \n 7 Salir \n")
     n=int(input("Elija una opcion"))
 print("*****FIN, GRACIAS*****")
